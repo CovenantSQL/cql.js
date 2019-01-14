f29522e41bc9679550677738dafcff4c9225567c
foreseaz <foreseazhang@gmail.com>
2019-01-14 20:17:33 +0800

    Concise keystore class

--- src/libs/keystore.ts
+++ src/libs/keystore.ts
@@ -1,7 +1,3 @@
 import Keyvenant from 'keyvenant'
 
-export default class Keystore extends Keyvenant {
-  constructor(props?) {
-    super(props)
-  }
-}
+export default class Keystore extends Keyvenant {}
